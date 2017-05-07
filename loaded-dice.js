f7e61a1e964a23229e6d3dfb575ab817476cc5d5
rickyforr <richard.forrester@gmail.com>
2017-05-07 21:19:06 +0000

    adding loaded dice exercise

---
+++ loaded-dice.js
@@ -0,0 +1,16 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  /* your code here */
+
+  return function() {
+    roll = list[i] + 1
+    return roll;
+    /* your code here */
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
